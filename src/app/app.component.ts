bd2430c64b56e7663e984ef337bb37aff3e146cd
usamaghazan <usamaghazan@yahoo.com>
2024-05-30 12:28:48 +0500

    Hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
